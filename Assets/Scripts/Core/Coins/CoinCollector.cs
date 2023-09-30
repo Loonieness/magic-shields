709eb2a9d46ee09a0d27d4688cb3508fb966704c
joabalmeida <joabalmeida.dev@gmail.com>
2023-09-30 00:02:52 -0300

    cliente já pega moedas

---
+++ Assets/Scripts/Core/Coins/CoinCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinCollector : NetworkBehaviour
+{
+    public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        //if it can't get the component
+        if(!col.TryGetComponent<Coin>(out Coin coin)) { return; }
+
+        int coinValue = coin.Collect();
+        
+        if(!IsServer) { return; }
+
+        TotalCoins.Value += coinValue;
+    }
+}
