4888103998ab164b8a7dcc84e113c463986b38fd
joabalmeida <joabalmeida.dev@gmail.com>
2023-10-17 23:38:33 -0300

    campo para nome de jogador feito

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;  
+    [SerializeField] private Button connectButton;
+    [SerializeField] private int minNameLength = 1;
+    [SerializeField] private int maxNameLength = 12;
+
+    private const string PlayerNameKey = "PlayerName";
+
+    private void Start()
+    {   //if we are a headless or dedicated server, just skip this scene
+        if(SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+
+        //you can already have a name registered before, so it will appear or, if not, the box comes empty
+        nameField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+        HandleNameChanged();
+    }
+
+    public void HandleNameChanged(){
+        //the connect button is only avaliable when these two conditions are met
+        connectButton.interactable =
+            nameField.text.Length >= minNameLength &&
+            nameField.text.Length <= maxNameLength;
+    }
+
+    public void Connect() {
+        PlayerPrefs.SetString(PlayerNameKey, nameField.text);
+
+        //load next scene, whatever it is
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
