3ed5b77f8cef914f4f89e3b8369b6e254e7afd5d
joabalmeida <joabalmeida.dev@gmail.com>
2023-10-17 21:58:10 -0300

    lobbies são criados agora

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 //remove monobehavior because this is only going to be a C# class that we create an instance of in the host singleton
 public class HostGameManager
 {
     private string joinCode;
+    private string lobbyId;
 
     private Allocation allocation;
 
@@ -53,8 +57,50 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            //Data is everything that everyone on the lobby can read. In this case, the joinCode
+            lobbyOptions.Data = new Dictionary<string, DataObject>(){
+                {
+                    "JoinCode", new DataObject(
+                        //they will join the lobby, and then be able to read the code as a member
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                    )
+                }
+
+            };
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
+                "My Lobby", MaxConnections, lobbyOptions);
+
+            lobbyId = lobby.Id;      
+            //We will call the HostSingleton, which is a monobehavior, and make it run the heartbeat every 15 seconds
+            //coroutines happen in monobehaviors, and this class isn't one. 
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));      
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
         NetworkManager.Singleton.StartHost();
         
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+    }
+
+    //IEnumerator is a coroutine, which will happen, pause, then happen again from where it stopped
+    private IEnumerator HeartBeatLobby(float waitTimeSeconds){
+        //this is an infinite loop
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+
+        while(true){
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+
+        }
+
     }
 }
