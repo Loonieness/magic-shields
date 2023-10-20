bae8de17551795979c0eb903737c690657177c04
joabalmeida <joabalmeida.dev@gmail.com>
2023-10-20 02:33:17 -0300

    leaderboard mostra os nomes certinho

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,6 +11,7 @@ public class Leaderboard : NetworkBehaviour
     [SerializeField] private LeaderboardEntityDisplay leaderboardEntityPrefab;
 
     private NetworkList<LeaderboardEntityState> leaderboardEntities;
+    private List<LeaderboardEntityDisplay> entityDisplays = new List<LeaderboardEntityDisplay>();
 
     private void Awake() {
         leaderboardEntities = new NetworkList<LeaderboardEntityState>();
@@ -56,10 +58,41 @@ public class Leaderboard : NetworkBehaviour
     {
         switch(changeEvent.Type){
             case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
-                Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
-                break;
-            case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+            //if there's not any entityDisplays where it has a ClientId where it is equal to this new ClientId comming
+            //spawn a new one. If match, don't spawn
+            if(!entityDisplays.Any(x => x.ClientId == changeEvent.Value.ClientId)){
+                LeaderboardEntityDisplay leaderboardEntity = 
+                    Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                leaderboardEntity.Initialise(
+                    changeEvent.Value.ClientId, 
+                    changeEvent.Value.PlayerName,
+                    changeEvent.Value.Coins);
+                entityDisplays.Add(leaderboardEntity); 
+            }
             break;
+
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+            LeaderboardEntityDisplay displayToRemove = 
+                entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+
+            if(displayToRemove != null){
+                //if we just destroy it, it can cause some issues. So first, dettach it from leaderboard
+                displayToRemove.transform.SetParent(null);
+                Destroy(displayToRemove.gameObject);
+                entityDisplays.Remove(displayToRemove);
+            }
+            break;
+
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Value:
+            LeaderboardEntityDisplay displayToUpdate = 
+                entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+
+            if(displayToUpdate != null){
+                displayToUpdate.UpdateCoins(changeEvent.Value.Coins);
+            }
+            break;
+
+
 
         }
     }
